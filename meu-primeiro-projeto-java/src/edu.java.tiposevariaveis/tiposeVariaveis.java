a5287a5de2efe485b56a52cff8135a2d2d64b6a6
Matheus Santos <eusoumathe@gmail.com>
2024-04-22 20:06:23 -0300

    tipos de variaveis e um if pra testar

--- meu-primeiro-projeto-java/src/edu.java.tiposevariaveis/tiposeVariaveis.java
+++ meu-primeiro-projeto-java/src/edu.java.tiposevariaveis/tiposeVariaveis.java
@@ -1,4 +1,21 @@
 public class tiposeVariaveis {
 
-    
-}
+    public static void main(String[] args) {
+        
+        //tipos de variaveis
+
+        int numero = 912182112;
+        double numeroFlutuante = 0.42;
+        String nomeDev = "Matheus";
+        boolean dadoVerdadeiro = true;
+
+        if (numero == 1) 
+            System.out.println("Num é que é mermo;");
+
+        else
+            System.out.println("Olá Mundo");
+        }
+
+
+    }
+
